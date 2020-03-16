630bb2e8560708d7b87ea9bc3951fe297fce82ec
Muneeb <mnbsd417@gmail.com>
2020-03-16 12:12:57 +0000

    First commit

--- src/Startup.java
+++ src/Startup.java
@@ -1,5 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Startup {
     public static void main(String[] args) {
-        
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Muneeb Asad\\Desktop\\jar_files\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("http://tutorialsninja.com/demo/index.php?route=common/home/");
+        //driver.close();
     }
 }
