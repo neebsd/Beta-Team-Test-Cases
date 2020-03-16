a7fc570660f2a5351e0c1a3061effdce47699db7
Muneeb <mnbsd417@gmail.com>
2020-03-16 19:02:12 +0000

    Adding rest of the desktop tests - need to troublshoot.

--- src/DesktopTests.java
+++ src/DesktopTests.java
@@ -7,17 +7,36 @@ public class DesktopTests {
     public static void initiate(WebDriver driver){
         desktop_01(driver);
         desktop_02(driver);
+        desktop_03(driver);
+        desktop_04(driver);
     }
 
-    public static void desktop_01(WebDriver driver){
-        WebElement desktopDrop = driver.findElement(By.xpath("//*[@id=\"menu\"]/div[2]/ul/li[1]/a"));   //Finding the desktop menu
+    public static void desktop_01(WebDriver driver){    //Testing drop down menu
+        WebElement desktopDrop = driver.findElement(By.xpath("//*[@id=\"menu\"]/div[2]/ul/li[1]/a"));
 
         Actions action = new Actions(driver);
         action.moveToElement(desktopDrop).perform();    //Making mouse hover over menu
     }
 
-    public static void desktop_02(WebDriver driver){
-        WebElement PCLink = driver.findElement(By.xpath("//*[@id=\"menu\"]/div[2]/ul/li[1]/div/div/ul/li[1]/a"));   //Finding PC link
+    public static void desktop_02(WebDriver driver){    //Testing PC link
+        driver.get("http://tutorialsninja.com/demo/index.php?route=common/home");   //Resetting back to home page
+        WebElement PCLink = driver.findElement(By.xpath("//*[@id=\"menu\"]/div[2]/ul/li[1]/div/div/ul/li[1]/a"));
+
+        Actions action = new Actions(driver);
+        action.moveToElement(PCLink).click().perform();     //Clicking on link
+    }
+
+    public static void desktop_03(WebDriver driver){    //Testing Mac link
+        driver.get("http://tutorialsninja.com/demo/index.php?route=common/home");   //Resetting back to home page
+        WebElement PCLink = driver.findElement(By.xpath("//*[@id=\"menu\"]/div[2]/ul/li[1]/div/div/ul/li[2]/a"));
+
+        Actions action = new Actions(driver);
+        action.moveToElement(PCLink).click().perform();     //Clicking on link
+    }
+
+    public static void desktop_04(WebDriver driver){    //Testing show all link
+        driver.get("http://tutorialsninja.com/demo/index.php?route=common/home");   //Resetting back to home page
+        WebElement PCLink = driver.findElement(By.xpath("//*[@id=\"menu\"]/div[2]/ul/li[1]/div/a"));
 
         Actions action = new Actions(driver);
         action.moveToElement(PCLink).click().perform();     //Clicking on link
