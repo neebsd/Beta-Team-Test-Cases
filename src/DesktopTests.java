9a81f6885149c1830d6a59676657675b9139434b
Muneeb <mnbsd417@gmail.com>
2020-03-16 14:02:45 +0000

    Adding comments

--- src/DesktopTests.java
+++ src/DesktopTests.java
@@ -10,16 +10,16 @@ public class DesktopTests {
     }
 
     public static void desktop_01(WebDriver driver){
-        WebElement desktopDrop = driver.findElement(By.xpath("//*[@id=\"menu\"]/div[2]/ul/li[1]/a"));
+        WebElement desktopDrop = driver.findElement(By.xpath("//*[@id=\"menu\"]/div[2]/ul/li[1]/a"));   //Finding the desktop menu
 
         Actions action = new Actions(driver);
-        action.moveToElement(desktopDrop).perform();
+        action.moveToElement(desktopDrop).perform();    //Making mouse hover over menu
     }
 
     public static void desktop_02(WebDriver driver){
-        WebElement PCLink = driver.findElement(By.xpath("//*[@id=\"menu\"]/div[2]/ul/li[1]/div/div/ul/li[1]/a"));
+        WebElement PCLink = driver.findElement(By.xpath("//*[@id=\"menu\"]/div[2]/ul/li[1]/div/div/ul/li[1]/a"));   //Finding PC link
 
         Actions action = new Actions(driver);
-        action.moveToElement(PCLink).click().perform();
+        action.moveToElement(PCLink).click().perform();     //Clicking on link
     }
 }
